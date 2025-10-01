38bebf4605cc493122235eb8ba98009bd36a85c5
luc4as-calixto <lucas_calixto-souza@estudante.sesisenai.org.br>
2025-10-01 17:26:09 -0300

    implementado conexão com broker MQTT'

--- MQTT_basic/MQTT_basic.ino
+++ MQTT_basic/MQTT_basic.ino
@@ -1,7 +1,14 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 const String SSID = "iPhone";
 const String PSWD = "iot_sul_123";
+
+const String brokerUrl = "test.mosquitto.org";     // URL do broker   (servidor)
+const int port = 1883;                             // Porta do broker (servidor)
+
+WiFiClient espClient;                              // Criando Cliente WiFi
+PubSubClient mqttClient(espClient);                // Criando Cliente MQTT
 
 // SSID = NOME
 // RSSI = Intesidade do sinal
@@ -11,6 +18,15 @@ void connectToWiFi();
 void setup() {
   Serial.begin(115200);
   connectToWiFi();
+  Serial.println("\nConectando ao broker");
+  mqttClient.setServer(brokerUrl.c_str(), port);
+  String userId = "ESP-CALIXTO";
+  mqttClient.connect(userId.c_str());
+  while (!mqttClient.connected()) {
+    Serial.println("Erro de conexão");
+    delay(500);
+  }
+  Serial.println("Conectado com sucesso!");
 }
 
 void loop() {
@@ -18,6 +34,7 @@ void loop() {
     connectToWiFi();
   } 
   delay(2000); 
+  mqttClient.loop();
 }
 
 
