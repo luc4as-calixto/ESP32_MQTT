561ca437a699172483c1fa21033341f07aadd31c
luc4as-calixto <lucas_calixto-souza@estudante.sesisenai.org.br>
2025-10-01 16:12:42 -0300

    implementada conexão simples WiFi

--- WiFi_test/WiFi_test.ino
+++ WiFi_test/WiFi_test.ino
@@ -1,20 +1,29 @@
 #include <WiFi.h>
+const String SSID = "iPhone";
+const String PSWD = "iot_sul_123";
 
 // SSID = NOME
 // RSSI = Intesidade do sinal
 
 void scanLocalNetworks();
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("Iniciando Scan de redes Wi-Fi");
   scanLocalNetworks();
+  Serial.println("Iniciando conexão com rede WiFi");
+  WiFi.begin(SSID, PSWD);
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.print("\nConectado!");
 }
 
 void loop() {
-
 }
 
 void scanLocalNetworks() {
+  Serial.println("Iniciando Scan de redes Wi-Fi");
   int number = WiFi.scanNetworks();
   delay(500);
   if (number == -1) {
